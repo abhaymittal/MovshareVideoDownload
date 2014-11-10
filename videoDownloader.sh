b32826d93629f17351276fa8cadf5159d9a18c97
Abhay Mittal <abhaymittal@y7mail.com>
2014-11-10 23:05:47 +0530

    Added support for mirrors.. Add all the download links in a quoted string

--- videoDownloader.sh
+++ videoDownloader.sh
@@ -3,53 +3,63 @@
 # Bash scripts accepts a movshare url and downloads the corresponding video using aria2c
 
 # Author: Abhay Mittal
-url=$1
-page_source=$(curl -s "$url") # Grab the html source
-
-#Grab the video site domain name
-domain=$(echo "$page_source" | grep "domain=");
-domain=${domain:`expr index "$domain" \"`}
-domain=${domain:0:${#domain}-2}
-echo $domain
-
-#Grab the necessary parameters to identify video"
-filekey=$(echo "$page_source" | grep "filekey")
-file=$(echo "$page_source" | grep "file=")
+urls=$1
+target_urls=""
 
 
-filekey=${filekey:`expr index "$filekey" =`}
-filekey=${filekey:0:${#filekey}-1}
+for url in $urls
+do
+    page_source=$(curl -s "$url") # Grab the html source
 
-#Check if the filekey is stored indirectly in a variable. Happens in case of nowvideo
-if [[ $filekey != \"* ]]
-then
-    filekey=$(echo "$page_source" | grep "var $filekey=")
+    #Grab the video site domain name
+    domain=$(echo "$page_source" | grep "domain=");
+    domain=${domain:`expr index "$domain" \"`}
+    domain=${domain:0:${#domain}-2}
+    echo $domain
+
+    #Grab the necessary parameters to identify video"
+    filekey=$(echo "$page_source" | grep "filekey")
+    file=$(echo "$page_source" | grep "file=")
+
+
     filekey=${filekey:`expr index "$filekey" =`}
     filekey=${filekey:0:${#filekey}-1}
-    
-fi
-filekey=${filekey:`expr index "$filekey" \"`}
-filekey=${filekey:0:${#filekey}-1}
-filekey=${filekey//./%2E}
 
-file=${file:`expr index "$file" \"`}
-file=${file:0:${#file}-2}
+    #Check if the filekey is stored indirectly in a variable. Happens in case of nowvideo
+    if [[ $filekey != \"* ]]
+    then
+	filekey=$(echo "$page_source" | grep "var $filekey=")
+	filekey=${filekey:`expr index "$filekey" =`}
+	filekey=${filekey:0:${#filekey}-1}
+	
+    fi
+    filekey=${filekey:`expr index "$filekey" \"`}
+    filekey=${filekey:0:${#filekey}-1}
+    filekey=${filekey//./%2E}
+
+    file=${file:`expr index "$file" \"`}
+    file=${file:0:${#file}-2}
 
 
-echo "FILEKEY = $filekey"
-echo "FILE = $file"
+    echo "FILEKEY = $filekey"
+    echo "FILE = $file"
 
 
-#Generate the target url
-target_url=$(lynx -source  "$domain/api/player.api.php?key=$filekey&file=$file")
-target_url=${target_url:4}
-target_url=${target_url:0:`expr index "$target_url" \&`-1}
-echo "TARGET URL = $target_url"
+    #Generate the target url
+    target_url=$(lynx -source  "$domain/api/player.api.php?key=$filekey&file=$file")
+    target_url=${target_url:4}
+    target_url=${target_url:0:`expr index "$target_url" \&`-1}
+    echo "TARGET URL = $target_url"
+    target_urls=$(echo "$target_urls $target_url")
+done
+
 
 #Grab File Name
 file_name=$(echo $page_source | xmllint --html --xpath '/html/head/title/text()' - 2> /dev/null )
 file_name=${file_name// /_}
 file_name=$(echo "$file_name.flv")
 
+
+
 #Download the file
-aria2c -o "$file_name" -x 5 -s 5 "$target_url"
+aria2c -o "$file_name" -x 5 -s 5 "$target_urls"
